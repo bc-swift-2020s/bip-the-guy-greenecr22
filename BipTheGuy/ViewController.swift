7a8230fcf37faac9c4197d566bc6525d54ddc6f5
Christopher Greene <greenecr@bc.edu>
2020-02-03 15:54:41 -0500

    Initial Commit

---
+++ BipTheGuy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BipTheGuy
+//
+//  Created by Christopher Greene on 2/3/20.
+//  Copyright © 2020 Christopher Greene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
